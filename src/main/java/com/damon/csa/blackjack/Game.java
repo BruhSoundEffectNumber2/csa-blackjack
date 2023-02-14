ff7b10e9db3067d34850e44f97df4c2ce1d135c8
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2023-02-13 17:24:17 -0800

    Main now calls game

--- src/main/java/com/damon/csa/blackjack/Game.java
+++ src/main/java/com/damon/csa/blackjack/Game.java
@@ -6,16 +6,54 @@ public class Game {
   ///////////////////////////////
 
   public static final int STARTING_CASH = 100;
+  public static final int MIN_BET = 5;
+
+  private InputManager input;
 
   private int numPlayers;
   private Player[] players;
+
+  /*
+   * While the dealer and player's are both instances of the Player class,
+   * they maintain very different rules on how they are used by the Game,
+   * hence the split.
+   */
+
+  private Player dealer;
+
+  private Deck deck;
 
   ///////////////////////////////
   // Constructor
   ///////////////////////////////
 
   public Game(int numPlayers) {
+    input = new InputManager(System.in);
+
     this.numPlayers = numPlayers;
     players = new Player[numPlayers];
+
+    createPlayers();
+
+    start();
+  }
+
+  private void createPlayers() {
+    dealer = new Player("Gus 'Dealer' Reiber", true);
+
+    for (int i = 0; i < numPlayers; i++) {
+      System.out.println("Player " + (i + 1) + ", what is your name?");
+      String name = input.getTrimmedText();
+
+      players[i] = new Player(name, false);
+
+      System.out.println("Hello " + name + ".");
+    }
+  }
+
+  private void start() {
+    deck = new Deck();
+    deck.shuffle();
+
   }
 }
--- src/main/java/com/damon/csa/blackjack/Main.java
+++ src/main/java/com/damon/csa/blackjack/Main.java
@@ -2,7 +2,11 @@ package com.damon.csa.blackjack;
 
 public class Main {
   public static void main(String[] args) {
-    Deck deck = new Deck();
-    deck.shuffle();
+    /*
+     * Right now, we only care about the actual logic of Blackjack,
+     * so supporting multiple players is irrelevant. In the future,
+     * that topic may be discussed, but there are many options.
+     */
+    new Game(1);
   }
 }
